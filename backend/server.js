fd373089c47ef919a69a55e4de020bbbb58656b7
CharbelBG <charbel.boughazale@gmail.com>
2022-10-12 08:01:56 +0300

    ready to deploy on heroku

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const cors = require('cors');
 app.use(cors());
@@ -8,8 +9,11 @@ const port = 3000;
 
 
 app.use('/', require('./routes/validationRoutes'));
-
-
+//setup our static folder
+app.use(express.static(path.join(__dirname,'../frontend/dist')));
+app.get('*', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'../', 'frontend', 'dist', 'index.html'))
+})
 app.listen(port,()=>{
     console.log(`sevrer is alive at ${port}`);
 });
